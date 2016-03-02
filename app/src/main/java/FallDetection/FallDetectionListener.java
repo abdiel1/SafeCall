ae2b866166bec134f9b6837d69b93549a783b6e6
Abdiel Rosado <abdielrosado@Abdiels-MacBook-Pro-2.local>
2016-03-02 15:38:48 -0400

    Created the FallDetectionListener and FallDetector interfaces

---
+++ app/src/main/java/FallDetection/FallDetectionListener.java
@@ -0,0 +1,12 @@
+package FallDetection;
+
+/**
+ * Created by abdielrosado on 3/2/16.
+ */
+public interface FallDetectionListener {
+
+    /**
+     * This method gets called when a FallDetector detects a fall.
+     */
+    public void onFallDetected();
+}
---
+++ app/src/main/java/FallDetection/FallDetector.java
@@ -0,0 +1,28 @@
+package FallDetection;
+
+/**
+ * Created by abdielrosado on 3/2/16.
+ */
+public interface FallDetector {
+
+    /**
+     * Register FallDetectionListener objects that want to know when a fall is detected.
+     * @param fallDetectionListener - FallDetectionListener object that wants to register.
+     */
+    public void registerFallDetectionListener(FallDetectionListener fallDetectionListener);
+
+
+    /**
+     * Remove FallDetectionListener objects that no longer want to know when a fall is detected.
+     * @param fallDetectionListener - FallDetectionListener object that wants to be removed.
+     * @return - Whether the FallDetectionListener object was removed.
+     */
+    public boolean removeFallDetectionListener(FallDetectionListener fallDetectionListener);
+
+    /**
+     * Notify all registered FallDetection listeners that a fall has been detected.
+     */
+    public void notifyFallDetectionListeners();
+
+
+}
