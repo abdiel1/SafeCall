25144c80a66ed517eaf3c7a2d350d25e6a538ba4
abdielrosado <abdiel.rosado@upr.edu>
2016-03-03 15:43:47 -0400

    Implemented ContactList, ContactListManager and Contact

---
+++ app/src/main/java/ContactManagement/Contact.java
@@ -0,0 +1,36 @@
+package ContactManagement;
+
+/**
+ * Created by abdielrosado on 3/2/16.
+ */
+public class Contact {
+
+    private String name;
+
+    private String phoneNumber;
+
+    public Contact(String name, String phoneNumber){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+
+    public String getPhoneNumber() {
+
+        return phoneNumber;
+    }
+
+
+}
---
+++ app/src/main/java/ContactManagement/ContactListManager.java
@@ -0,0 +1,117 @@
+package ContactManagement;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by abdielrosado on 3/3/16.
+ * */
+public class ContactListManager implements ContactList{
+
+    private List<Contact> contactList;
+
+    private final static String contactsFile = "EmergencyContacts.txt";
+
+    private static ContactListManager instance;
+
+    private ContactListManager(){
+        contactList = new ArrayList<Contact>();
+    }
+
+    public ContactListManager getInstance(){
+        if(instance == null){
+            instance = new ContactListManager();
+        }
+        return instance;
+    }
+
+
+    @Override
+    public List<Contact> getContactList() {
+        return contactList;
+    }
+
+    @Override
+    public void addContact(Contact contact) {
+        if(!contactList.contains(contact)){
+            contactList.add(contact);
+        }
+    }
+
+    @Override
+    public boolean removeContact(Contact contact) {
+
+        return contactList.remove(contact);
+    }
+
+    public void reorderContact(Contact contact, int position){
+        if(position < contactList.size() && position >= 0){
+            if(contactList.contains(contact)){
+                contactList.remove(contact);
+                contactList.add(position,contact);
+            }
+        } else{
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    private void loadContacts(){
+        File file = new File(contactsFile);
+        try{
+            BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+            try{
+                JSONTokener tokener;
+                JSONObject jsonObject;
+                String line;
+                Contact contact;
+
+                while ((line = in.readLine()) != null){
+                    tokener = new JSONTokener(line);
+                    jsonObject = (JSONObject) tokener.nextValue();
+                    contact = new Contact(jsonObject.get("Name").toString(),
+                            jsonObject.get("Number").toString());
+                    contactList.add(contact);
+                }
+            } catch (JSONException e){
+
+            }
+        } catch(IOException e){
+
+        }
+    }
+
+    public void saveContacts(){
+
+        JSONObject jsonObject;
+        File file = new File(contactsFile);
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            try {
+                for (Contact c : contactList) {
+                    jsonObject = new JSONObject();
+                    jsonObject.put("Name", c.getName());
+                    jsonObject.put("Number", c.getPhoneNumber());
+
+                    fileWriter.write(jsonObject.toString() + "\n");
+                }
+            } catch (JSONException | IOException e) {
+
+            } finally {
+                if (fileWriter != null) {
+                    fileWriter.close();
+                }
+            }
+        } catch (IOException e){
+
+        }
+    }
+}
