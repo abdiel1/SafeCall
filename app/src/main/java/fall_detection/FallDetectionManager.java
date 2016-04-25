8d3c48c2aee9df15c1c4141fa8aff7407427464c
abdielrosado <abdiel.rosado@upr.edu>
2016-04-24 22:10:22 -0400

    Modified fall detection

--- app/src/main/java/fall_detection/FallDetectionManager.java
+++ app/src/main/java/fall_detection/FallDetectionManager.java
@@ -11,6 +11,7 @@ import org.jscience.mathematics.vector.DenseMatrix;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Created by abdielrosado on 3/2/16.
@@ -25,8 +26,9 @@ public class FallDetectionManager implements FallDetector, SensorEventListener {
 
     //private final double[][] FALL_INVERSE_COVARIANCE_VALUES = {{382.3,-258.2,15.8},{-258.2,756.1,67.9},{15.8,67.9,578.3}};
 
-    private final Real[][] FALL_INVERSE_COVARIANCE_VALUES = {{Real.valueOf(0.0003823), Real.valueOf(-0.0002582), Real.valueOf(0.0000158)},
-            {Real.valueOf(-0.0002582), Real.valueOf(0.0007561), Real.valueOf(0.0000679)}, {Real.valueOf(0.0000158), Real.valueOf(0.0000679), Real.valueOf(0.0005783)}};
+    private final Real[][] FALL_INVERSE_COVARIANCE_VALUES = {{Real.valueOf(0.0002447302456), Real.valueOf(-0.0000214938914933133), Real.valueOf(-0.00000718507564204711)},
+            {Real.valueOf(-0.0000214938914933133), Real.valueOf(0.000126294693009017), Real.valueOf(-0.0000182498286063061)},
+            {Real.valueOf(-0.00000718507564204711), Real.valueOf(-0.0000182498286063061), Real.valueOf(0.000170484778924723)}};
 
     private final DenseMatrix<Real> FALL_INVERSE_COVARIANCE_MATRIX = DenseMatrix.valueOf(FALL_INVERSE_COVARIANCE_VALUES);
 
@@ -35,16 +37,17 @@ public class FallDetectionManager implements FallDetector, SensorEventListener {
 //    private final double[][] NON_FALL_INVERSE_COVARIANCE_VALUES = {{0.0132,-0.0036,-0.0047},{-0.0036,0.0054,-0.0001},
 //            {-0.0047,-0.0001,0.0108}};
 
-    private final Real[][] NON_FALL_INVERSE_COVARIANCE_VALUES = {{Real.valueOf(0.0132), Real.valueOf(-0.0036), Real.valueOf(-0.0047)},
-            {Real.valueOf(-0.0036), Real.valueOf(0.0054), Real.valueOf(-0.0001)}, {Real.valueOf(-0.0047), Real.valueOf(-0.0001), Real.valueOf(0.0108)}};
-
-    private final DenseMatrix<Real> NON_FALL_INVERSE_COVARIANCE_MATRIX = DenseMatrix.valueOf(NON_FALL_INVERSE_COVARIANCE_VALUES);
+//    private final Real[][] NON_FALL_INVERSE_COVARIANCE_VALUES = {{Real.valueOf(0.00584466950764467), Real.valueOf(-0.001246764431), Real.valueOf(-0.001718949159)},
+//            {Real.valueOf(-0.001246764431), Real.valueOf(0.003938964257), Real.valueOf(-0.0008250059424)},
+//            {Real.valueOf(-0.001718949159), Real.valueOf(-0.0008250059424), Real.valueOf(0.004164093876)}};
+//
+//    private final DenseMatrix<Real> NON_FALL_INVERSE_COVARIANCE_MATRIX = DenseMatrix.valueOf(NON_FALL_INVERSE_COVARIANCE_VALUES);
 
     // private final RealMatrix NON_FALL_INVERSE_COVARIANCE_MATRIX = new Array2DRowRealMatrix(NON_FALL_INVERSE_COVARIANCE_VALUES);
 
     //private final double[] FALL_MEANS = {78.5251,52.283,61.5768};
 
-    private final Real[][] FALL_MEANS = {{Real.valueOf(78.5251)}, {Real.valueOf(52.283)}, {Real.valueOf(61.5768)}};
+    private final Real[][] FALL_MEANS = {{Real.valueOf(95.93624633)}, {Real.valueOf(102.222734)}, {Real.valueOf(94.7043037)}};
 
     //private final RealMatrix FALL_MEANS_MATRIX = new Array2DRowRealMatrix(FALL_MEANS);
 
@@ -52,18 +55,20 @@ public class FallDetectionManager implements FallDetector, SensorEventListener {
 
     //private final double[] NON_FALL_MEANS = {10.0271,11.8262,10.1568};
 
-    private final Real[][] NON_FALL_MEANS = {{Real.valueOf(10.0271)}, {Real.valueOf(11.8262)}, {Real.valueOf(10.1568)}};
+//    private final Real[][] NON_FALL_MEANS = {{Real.valueOf(13.3182)}, {Real.valueOf(14.1203)}, {Real.valueOf(13.6385)}};
+//
+//    //private final RealMatrix NON_FALL_MEANS_MATRIX = new Array2DRowRealMatrix(NON_FALL_MEANS);
+//
+//    private final DenseMatrix<Real> NON_FALL_MEANS_MATRIX = DenseMatrix.valueOf(NON_FALL_MEANS);
 
-    //private final RealMatrix NON_FALL_MEANS_MATRIX = new Array2DRowRealMatrix(NON_FALL_MEANS);
+    private AtomicBoolean alarmOn;
 
-    private final DenseMatrix<Real> NON_FALL_MEANS_MATRIX = DenseMatrix.valueOf(NON_FALL_MEANS);
-
-    private volatile boolean alarmOn;
+    private Real multiplyFactor;
 
     private static FallDetectionManager instance;
 
     private FallDetectionManager() {
-        alarmOn = false;
+        alarmOn = new AtomicBoolean(false);
     }
 
     public static FallDetectionManager getInstance() {
@@ -112,17 +117,26 @@ public class FallDetectionManager implements FallDetector, SensorEventListener {
             DenseMatrix<Real> sampleMatrix = DenseMatrix.valueOf(sample);
             Real fallMahalanobisDistance = calculateMahalanobisDistance(sampleMatrix, FALL_INVERSE_COVARIANCE_MATRIX,
                     FALL_MEANS_MATRIX);
-            Real nonFallMahalanobisDistance = calculateMahalanobisDistance(sampleMatrix, NON_FALL_INVERSE_COVARIANCE_MATRIX,
-                    NON_FALL_MEANS_MATRIX);
+//            Real nonFallMahalanobisDistance = calculateMahalanobisDistance(sampleMatrix, NON_FALL_INVERSE_COVARIANCE_MATRIX,
+//                    NON_FALL_MEANS_MATRIX);
 
 
-            if (fallMahalanobisDistance.compareTo(nonFallMahalanobisDistance) == -1 && !alarmOn) {
-                Log.d("Fall", "Fall was detected");
-                //Log.d("FallMD",fallMahalanobisDistance.toString());
-                //Log.d("NFallMD",nonFallMahalanobisDistance.toString());
-                alarmOn = true;
-                notifyFallDetectionListeners();
+
+            Log.d("Mahalanobis",fallMahalanobisDistance.toString() + ", " + multiplyFactor.toString());
+
+            synchronized (this){
+                if ((fallMahalanobisDistance.doubleValue() < 1) && !alarmOn.get()) {
+                    Log.d("Fall", "Fall was detected");
+                    //Log.d("FallMD",fallMahalanobisDistance.toString());
+                    //Log.d("NFallMD",nonFallMahalanobisDistance.toString());
+                    FallDetectionManagement.stopListening();
+                    alarmOn.set(true);
+                    previousValues[0] = 0;
+                    notifyFallDetectionListeners();
+                }
             }
+
+
         } else {
             previous_time = System.currentTimeMillis();
             previousValues[0] = event.values[0];
@@ -130,6 +144,7 @@ public class FallDetectionManager implements FallDetector, SensorEventListener {
             previousValues[2] = event.values[2];
 
         }
+
 
     }
 
@@ -143,7 +158,15 @@ public class FallDetectionManager implements FallDetector, SensorEventListener {
 
         Real time = Real.valueOf(System.currentTimeMillis());
         Real timeDifference = time.minus(Real.valueOf(previous_time));
-        Real multiplyFactor = Real.valueOf(10);
+
+
+
+        if (timeDifference.compareTo(Real.valueOf(100)) == -1) {
+            multiplyFactor = Real.valueOf(100);
+        } else {
+            multiplyFactor = Real.valueOf(1000);
+        }
+
 
         if (timeDifference.doubleValue() == 0) {
             return null;
@@ -152,16 +175,16 @@ public class FallDetectionManager implements FallDetector, SensorEventListener {
 
         Real accelerationDifference = Real.valueOf(values[0]).minus(Real.valueOf(previousValues[0]));
         Real slope = accelerationDifference.divide(timeDifference).times(multiplyFactor);
-        slopes[0] = slope.doubleValue();
+        slopes[0] = slope.abs().doubleValue();
 
         accelerationDifference = Real.valueOf(values[1]).minus(Real.valueOf(previousValues[1]));
         slope = accelerationDifference.divide(timeDifference).times(multiplyFactor);
-        slopes[1] = slope.doubleValue();
+        slopes[1] = slope.abs().doubleValue();
 
 
         accelerationDifference = Real.valueOf(values[2]).minus(Real.valueOf(previousValues[2]));
         slope = accelerationDifference.divide(timeDifference).times(multiplyFactor);
-        slopes[2] = slope.doubleValue();
+        slopes[2] = slope.abs().doubleValue();
 
         //String s = String.format("%s %s %s",values[0],values[1],values[2]);
 
@@ -191,7 +214,7 @@ public class FallDetectionManager implements FallDetector, SensorEventListener {
     }
 
     public void alarmOff() {
-        alarmOn = false;
+        alarmOn.set(false);
     }
 
 
